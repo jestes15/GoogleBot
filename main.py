f78955bc7d0218af0f2c554a5c5e784feb998ed3
jestes15 <sagepup0620@gmail.com>
2020-10-21 16:54:36 -0500

    Made the msgs for efficient

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
-from datetime import date
 import datetime
-from discord.ext import commands
-import discord
-from dotenv import load_dotenv
-import encryption_cmd as cmd
-from googlesearch import search
 import os
 import random as r
-import robin_stocks as robin
-from wolframclient.evaluation import WolframLanguageSession
-from time import sleep
-from googletrans import Translator
+from datetime import date
+import discord
 import googletrans
-
+import robin_stocks as robin
+from discord.ext import commands
+from dotenv import load_dotenv
+from googlesearch import search
+from googletrans import Translator
+from wolframclient.evaluation import WolframLanguageSession
+import encryption_cmd as cmd
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -45,7 +43,7 @@ async def load_stock(ctx, option=None, stocks=None, str3=None, everything=None):
         if option is None:
             msg = "Im sorry, but I need the symbol for the stock you are looking for, please try again. " \
                   "If you think I made a mistake, please contact <@!610469915442282526>  to resolve this issue"
-            await ctx.channel.send(msg)
+            await ctx.channel.send(f'{msg}')
 
         elif stocks == 'description':
             description = robin.stocks.get_fundamentals(option, 'description')
@@ -58,12 +56,12 @@ async def load_stock(ctx, option=None, stocks=None, str3=None, everything=None):
             if stock_info[0] is None:
                 msg = "Im sorry, but the stock you are looking for is not in the archives. " \
                       "If you think I made a mistake, please contact <@!610469915442282526> to resolve this issue"
-                await ctx.channel.send(msg)
+                await ctx.channel.send(f'{msg}')
             else:
                 stock_info_number = stock_info[0]
-                await ctx.channel.send('Price = $' + str(stock_info_number) + '\nP/B Ratio = ' + str(pb_ratio[0]) +
-                                       '\nP/E Ratio = ' + str(pe_ratio[0]) + '\nDividend Yield = ' +
-                                       str(dividend_yield[0]))
+                msg = 'Price = $' + str(stock_info_number) + '\nP/B Ratio = ' + str(pb_ratio[0]) + '\nP/E Ratio = ' + \
+                      str(pe_ratio[0]) + '\nDividend Yield = ' + str(dividend_yield[0])
+                await ctx.channel.send(f'{msg}')
 
     if option == 'common':
         if everything == 'everything':
@@ -73,9 +71,8 @@ async def load_stock(ctx, option=None, stocks=None, str3=None, everything=None):
             current_date = datetime.datetime.now()
             d2 = current_date.strftime("%m/%d/%Y %I:%M:%S %p\n")
             stock_list[0] = d2
-
-            await ctx.channel.send("Please wait while I compile the information you have "
-                                   "requested, approximately 2 to 3 seconds")
+            msg = "Please wait while I compile the information you have requested, approximately 2 to 3 seconds"
+            await ctx.channel.send(f'{msg}')
             await discord.channel.TextChannel.trigger_typing(self=ctx)
             num = 0
             sub = stock_list[0] + '\n'
@@ -88,7 +85,7 @@ async def load_stock(ctx, option=None, stocks=None, str3=None, everything=None):
                 sub += string[0:-1] + '\nPrice = $' + str(stock_price[0]) + '\nP/B Ratio = ' + str(pb_ratio[0])
                 sub += '\nP/E Ratio = ' + str(pe_ratio[0]) + '\nDividend Yield = ' + str(dividend_yield[0]) + '\n'
                 num += 1
-            await ctx.channel.send(sub)
+            await ctx.channel.send(f'{sub}')
 
         else:
             stock_list = {}
@@ -97,9 +94,8 @@ async def load_stock(ctx, option=None, stocks=None, str3=None, everything=None):
             current_date = datetime.datetime.now()
             d2 = current_date.strftime("%m/%d/%Y %I:%M:%S %p\n")
             stock_list[0] = d2
-
-            await ctx.channel.send("Please wait while I compile the information you have requested, approximately 2 "
-                                   "to 3 seconds")
+            msg = "Please wait while I compile the information you have requested, approximately 2 to 3 seconds"
+            await ctx.channel.send(f'{msg}')
             await discord.channel.TextChannel.trigger_typing(self=ctx)
             num = 0
             sub = stock_list[0] + '\n'
@@ -108,7 +104,7 @@ async def load_stock(ctx, option=None, stocks=None, str3=None, everything=None):
                 stock_price = robin.stocks.get_latest_price(array[num])
                 sub += string[0:-1] + ' = $' + str(stock_price[0]) + '\n'
                 num += 1
-            await ctx.channel.send(sub)
+            await ctx.channel.send(f'{sub}')
 
 
 @bot.command(name='add', help='Adds a symbol to the common stock list')
@@ -126,14 +122,14 @@ async def on_message(message):
     hello_dictionary = {
         1: 'Hello Mad Lad',
         2: 'Howdy Partner',
-        3: "'Ello Govena",
+        3: "Ello gov'na",
         4: "Sup Bruh",
         5: "こんにちは",
         6: "Go away",
         7: "ねじオフ"
     }
-    x = r.randint(1, 7)
-    await message.channel.send(hello_dictionary[x])
+    x = hello_dictionary[r.randint(1, 7)]
+    await message.channel.send(f'{x}')
     # Sends Hello mad lad after the user prompts it in discord
 
 
@@ -149,23 +145,30 @@ async def show(ctx, encryption: str, choices: str):
               'SHA512\n' \
               'blake2b\n' \
               'md5'
-    await ctx.channel.send("When using the encode command, choices are case sensitive.\n" + choices)
+    await ctx.channel.send(f'When using the encode command, choices are case sensitive.\n {choices}')
 
 
 @bot.command(name='encode', help='syntax: encode <String> using <choice of encryption>')
 async def encode(ctx, get_string: str, using: str, get_choice: str):
     encode_s = cmd.EncodeString
     encrypted_string = encode_s.switch(cmd.EncodeString(get_string), get_choice).string
-    await ctx.channel.send(encrypted_string)
+    await ctx.channel.send(f'{encrypted_string}')
 
 
 @bot.command(name='echo', help='Echos back user input')
 async def echo(ctx, user_input: str):
     if 'gay' in user_input.lower():
-        await ctx.channel.send("I'm sorry, that message is not allowed.")
+        msg = "I'm sorry, that message is not allowed."
+        await ctx.channel.send(f'{msg}')
+    elif 'homo' in user_input.lower():
+        msg = "I'm sorry, that message is not allowed."
+        await ctx.channel.send(f'{msg}')
+    elif 'fag' in user_input.lower():
+        msg = "I'm sorry, that message is not allowed."
+        await ctx.channel.send(f'{msg}')
     else:
         await discord.channel.TextChannel.trigger_typing(self=ctx)
-        await ctx.channel.send(user_input)
+        await ctx.channel.send(f'{user_input}')
 
 
 @bot.command(name='announce', help='Allows announcement to be used')
@@ -173,9 +176,9 @@ async def ann(ctx, user_input: str, channel_id: int, delete=False):
     await ctx.channel.send("Request received: executing now")
     channel = bot.get_channel(channel_id)
     if delete is True:
-        await channel.send(user_input + ' ' + str(delete))
+        await channel.send(f'{user_input} {str(delete)}')
     else:
-        await channel.send(user_input)
+        await channel.send(f'{user_input}')
     # Echos user input
 
 
@@ -189,7 +192,7 @@ async def meme(ctx, me: str, quotes: str):
         4: '*China*'
     }
     x = int(r.randint(1, 4))
-    await ctx.channel.send(meme_dictionary[x])
+    await ctx.channel.send(f'{meme_dictionary[x]}')
 
 audio_dict = {
     1: 'playme.mp3',
@@ -213,12 +216,13 @@ async def image(ctx, me: str, memes: str, type_f=None, choice=None):
         await ctx.channel.send(file=discord.File('Images/image.jpg'))
     if type_f is not None:
         if type_f == 'mp3':
-            await ctx.channel.send(file=discord.File('Audio/' + audio_dict[int(choice)]))
+            file = audio_dict[int(choice)]
+            await ctx.channel.send(file=discord.File(f'Audio/{file}'))
         if type_f == 'gif':
             await ctx.channel.send(file=discord.File('GIF/258c8822b6a11bdc8d0060bd9bb47df3.gif'))
     else:
         x1 = str(r.randint(6, 16))
-        await ctx.channel.send(file=discord.File('Images/image' + x1 + '.jpg'))
+        await ctx.channel.send(file=discord.File(f'Images/image{x1}.jpg'))
 
 
 @bot.command(name='search', help='syntax=for <book><chapter><start-verse><optional: end-verse>')
@@ -230,7 +234,8 @@ async def GetBibleVerse(ctx, for2: str, book_name: str, chapter: int, verse: int
 
         if last_verse == 0:
             verse_line = 2 * (verse - 1)
-            await ctx.channel.send(bible_read[verse_line])
+            msg = bible_read[verse_line]
+            await ctx.channel.send(f'{msg}')
 
         else:
             i = verse
@@ -242,12 +247,12 @@ async def GetBibleVerse(ctx, for2: str, book_name: str, chapter: int, verse: int
                 i += 1
             length = len(bible_verse)
             if length < 2001:
-                await ctx.channel.send(bible_verse)
+                await ctx.channel.send(f'{bible_verse}')
             else:
                 msg = "Error: You have requested too many Bible verses " \
                       "and have exceeded the discord limit of 2000 characters " \
                       "please shorten your request and try again."
-                await ctx.channel.send(msg)
+                await ctx.channel.send(f'{msg}')
 
 
 @bot.command(name='what', help='Returns the current date')
@@ -272,19 +277,19 @@ async def google(ctx, query: str):
     for j in search(query, tld="co.in", lang='en', num=10, stop=10, pause=2):
         results += "<" + j + ">\n"
 
-    await ctx.channel.send(results)
+    await ctx.channel.send(f'{results}')
 
 
 @bot.command(name='evaluate', help='evaluates line of code given')
 async def evaluate(ctx, code: str):
     g3 = eval(code)
-    await ctx.channel.send(g3)
+    await ctx.channel.send(f'{g3}')
 
 
 @bot.command(name='solve', help='Solves any function given in Wolfram Format')
 async def mathematica(ctx, function: str):
     evaluated = session.evaluate(function)
-    await ctx.channel.send('```\n' + str(evaluated) + '```')
+    await ctx.channel.send(f'```\n{evaluated}```')
 
 
 @bot.command(name='translate', help='Translates a string to another language')
@@ -297,9 +302,9 @@ async def translate(ctx, user_input: str, to: str, trans_lang: str):
         error_msg = 'The section of text you want to translate makes the message over 2000 characters, '\
                     'please shorten it'
         if len(msg) > 2000:
-            await ctx.channel.send(error_msg)
+            await ctx.channel.send(f'{error_msg}')
         else:
-            await ctx.channel.send(msg)
+            await ctx.channel.send(f'{msg}')
 
 
 @bot.command(name='roll', help='Rolls a dice')
@@ -307,6 +312,6 @@ async def roll(ctx, dice_type: str):
     dice_type1 = dice_type.lower()
     num = int(dice_type1.replace("d", ""))
     result = r.randint(1, num)
-    await ctx.channel.send(result)
+    await ctx.channel.send(f'{result}')
 
 bot.run(TOKEN)
