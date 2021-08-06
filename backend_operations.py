b28c5eadbcb3a5cf1a2df8de6b4f8fbe8b1dfe42
jestes15 <sagepup0620@gmail.com>
2021-08-05 22:51:06 -0500

    Added an on_ready event to notify forever that the bot has started

--- backend_operations.py
+++ backend_operations.py
@@ -28,6 +28,16 @@ if __name__ == "__main__":
 
     bot = commands.Bot(command_prefix=["$"])
 
+    @bot.event
+    async def on_ready():
+        channel = bot.get_channel(756568695320608910)  # Gets the channel with this channel id
+        print(f'{bot.user.name} has connected to Discord')  # Sends an online message to the command line
+        await bot.change_presence(activity=discord.Game(name="search and record"))
+        # Sends an online message to the aforementioned channel
+        message = await channel.send(f'{bot.user.name} has connected to discord')
+        embed_var = discord.Embed(title='Thank you for using me!', description="Powered by:", color=0xff000b)
+        embed_var.set_thumbnail(url='https://www.python.org/static/community_logos/python-logo-master-v3-TM.png')
+        await message.edit(embed=embed_var)  # Send a thank you message with a powered by Python message
 
     @has_permissions(administrator=True)
     @bot.command(name="shell-run")
